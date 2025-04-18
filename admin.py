145261576e8e4f0c82f0763c62a633e3139ab426
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2025-04-18 15:10:53 +0300

    add datetime fields to PokemonEntity.

--- admin.py
+++ admin.py
@@ -10,6 +10,15 @@ class PokemonAdmin(admin.ModelAdmin):
     list_display = ('title', 'image')
 
 class PokemonEntityAdmin(admin.ModelAdmin):
-    list_display = ('pokemon', 'lat', 'lon')
-    list_filter = ('pokemon',)
+    list_display = ('pokemon', 'lat', 'lon', 'appeared_at', 'disappeared_at')
+    list_filter = ('pokemon', 'appeared_at')
     search_fields = ('pokemon__title',)
+    fieldsets = (
+        (None, {
+            'fields': ('pokemon', ('lat', 'lon'))
+        }),
+        ('Временные параметры', {
+            'fields': ('appeared_at', 'disappeared_at'),
+            'classes': ('collapse',)
+        }),
+    )
