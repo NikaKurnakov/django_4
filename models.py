3b9eec7248724aa52637eccff222b82a04efb159
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2025-04-23 18:41:02 +0300

    add changes to pokemon page

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from django.core.validators import MinValueValidator
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='pokemon_images', null=True, blank=True)
+    description = models.TextField(verbose_name='Описание', blank=True)
 
     def __str__(self):
         return self.title
