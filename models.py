88453a26b2eb275bd7abfdc890a4ac90ccb7b840
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2025-04-23 18:56:55 +0300

    add new title in other lang

--- models.py
+++ models.py
@@ -3,9 +3,11 @@ from django.core.validators import MinValueValidator
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name='Название (рус)', blank=True)
     image = models.ImageField(upload_to='pokemon_images', null=True, blank=True)
     description = models.TextField(verbose_name='Описание', blank=True)
+    title_en = models.CharField(max_length=200, verbose_name='Название (англ)', blank=True)
+    title_jp = models.CharField(max_length=200, verbose_name='Название (яп)', blank=True)
 
     def __str__(self):
         return self.title
