c6b2fe2fe2c77423d173d2bcd9a6692f1d0c052d
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2025-04-25 15:30:47 +0300

    add a previous_evolution

--- models.py
+++ models.py
@@ -8,10 +8,17 @@ class Pokemon(models.Model):
     description = models.TextField(verbose_name='Описание', blank=True)
     title_en = models.CharField(max_length=200, verbose_name='Название (англ)', blank=True)
     title_jp = models.CharField(max_length=200, verbose_name='Название (яп)', blank=True)
+    previous_evolution = models.ForeignKey(
+        'self',
+        verbose_name='Из кого эволюционировал',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='next_evolutions'
+    )
 
     def __str__(self):
         return self.title
-
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='entities')
