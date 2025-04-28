60848130d9d0bdf77210e9c8684836e5974661d9
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2025-04-28 18:08:39 +0300

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -27,11 +27,11 @@ class PokemonEntity(models.Model):
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(verbose_name='Время появления', null=True, blank=True)
     disappeared_at = models.DateTimeField(verbose_name='Время исчезновения', null=True, blank=True)
-    level = models.IntegerField(verbose_name='Уровень', null=True, blank=True, validators=[MinValueValidator(1)])
-    health = models.IntegerField(verbose_name='Здоровье', null=True, blank=True, validators=[MinValueValidator(1)])
-    strength = models.IntegerField(verbose_name='Атака', null=True, blank=True, validators=[MinValueValidator(1)])
-    defence = models.IntegerField(verbose_name='Защита', null=True, blank=True, validators=[MinValueValidator(1)])
-    stamina = models.IntegerField(verbose_name='Выносливость', null=True, blank=True, validators=[MinValueValidator(1)])
+    level = models.PositiveSmallIntegerField(verbose_name='Уровень', null=True, blank=True)
+    health = models.PositiveIntegerField(verbose_name='Здоровье', null=True, blank=True)
+    strength = models.PositiveSmallIntegerField(verbose_name='Атака', null=True, blank=True)
+    defence = models.PositiveSmallIntegerField(verbose_name='Защита', null=True, blank=True)
+    stamina = models.PositiveSmallIntegerField(verbose_name='Выносливость', null=True, blank=True)
 
     def __str__(self):
         return f"{self.pokemon.title} (уровень {self.level or '?'})"
